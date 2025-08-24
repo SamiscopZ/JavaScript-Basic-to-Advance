9152579c9f196fe3d748f49cd4d6175492db09f4
Samiul Islam <samiul.srss@gmail.com>
2025-08-24 13:15:24 +0600

    Error handling used to fix bugs in codes.

---
+++ 12.errorHandling.js
@@ -0,0 +1,13 @@
+let a = prompt('Enter first number');
+let b = prompt('Enter second number');
+
+if (isNaN(a) || isNaN(b)) {
+  throw SyntaxError("You didn't input a number"); //Throw will show customed error
+}
+sum = parseInt(a) + parseInt(b);
+
+try {
+  console.log('The sum is ', sum * x);
+} catch {
+  console.log('Error comes');
+}
