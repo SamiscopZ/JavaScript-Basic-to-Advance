68150f17f7cc1ab20281e53f4fb89e91e5cc71d0
Samiul Islam <samiul.srss@gmail.com>
2025-08-24 13:12:57 +0600

    Important

---
+++ 11.RegExp.js
@@ -0,0 +1,14 @@
+//* Search & Match
+let text = "Please, let me know. But if you don't know me, you will suffer.";
+
+// console.log(text.search('me'));
+console.log(text.search(/me/i)); //To find from all the cases (UpperCase or LowerCase)
+console.log(text.match(/me/gi)); //To match same word though it is Upper or Lower case.
+
+//* replace
+let carDetails =
+  'BMW is one of the most expensive car in the world. If you want to afford a BMW you will have to be billioniare.';
+
+console.log(carDetails);
+console.log(carDetails.replace('BMW', 'Tesla')); //To replace a word.
+console.log(carDetails.replace(/BMW/gi, 'Tesla')); //To replace a same word in everywhere.
