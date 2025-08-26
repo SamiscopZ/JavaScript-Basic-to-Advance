c806d6f346006e53761376e7ac9a03dc65fd1c6c
Samiul Islam <samiul.srss@gmail.com>
2025-08-26 16:17:59 +0600

    Adding 01 and 02

---
+++ 01.dtypes.js
@@ -0,0 +1,50 @@
+//Data types
+/*
+ ? 1. Primitive
+ ? 2. Non-primitive
+ */
+
+/*
+ * Primitive has 7 types of data
+ */
+
+// String
+let color = 'yellow';
+let firstName = 'Mohammad';
+
+// Number
+let length = 15; //integer
+let weight = 90.5; //flot
+
+// Booleans
+let x = true;
+let y = false;
+
+//Bigint
+let z = BigInt('123456789012345678901234567890');
+
+//undefined
+let counter;
+console.log(counter);
+
+//null
+let dec = null;
+console.log(typeof dec, dec);
+
+//Symbol;
+
+/*
+ * Non-Primitive has 1 type of data
+ */
+
+//Object
+const person = {
+  firstName: 'Sami',
+  lastName: 'Islam',
+  age: 20,
+  phone: '016xxxxxxx0',
+  address: 'Jatrabari, Dhaka, Bangladesh',
+};
+person.firstName = 'Samiul';
+person.height = '5" 6';
+delete person.age;
---
+++ 02.str.js
@@ -0,0 +1,37 @@
+//charAt
+let num = 'Hello World';
+console.log(num.charAt(6));
+
+//concat
+let text = 'hello';
+let text1 = 'world';
+
+console.log(text.concat(' ', text1));
+
+//indexOf
+let str = "Hey! What's up";
+console.log(str.indexOf('up'));
+
+//length
+let str1 = "Hey! What's up";
+console.log(str1.length);
+
+//repeat
+let hablu = 'Please, return my money. ';
+console.log(hablu.repeat(10));
+
+//replace
+let nam = 'Sami Islam';
+console.log(nam.replace('Sami', 'Samiul'));
+
+// parseInt
+let n = '1000.87';
+console.log(parseInt(n)); //to convert string to number
+
+// parseFloat
+let nn = '1000.3456';
+console.log(parseFloat(nn));
+
+/*
+ ? parseInt and parseFloat are used to convert strings to numbers. parseInt removes the decimal part after the dot (.), but parseFloat keeps it.
+ */
