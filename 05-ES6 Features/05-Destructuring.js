17dd1f87cb0f4a49d32504df0aee959a7156b51f
Samiul Islam <samiul.srss@gmail.com>
2025-08-30 16:10:40 +0600

    Destructuring arrays and objects

---
+++ 05-ES6 Features/05-Destructuring.js
@@ -0,0 +1,43 @@
+//* Destruction
+
+// Destructuring allows to extract values from arrays or objects into variables in a clean way, making them shorter and easy to use.
+
+// Object destructing
+const user = {
+  id: 339,
+  name: 'Sami',
+  age: 21,
+  //   education: {
+  //     degree: 'MSC',
+  //   },
+};
+
+const { name: title } = user;
+
+const { education: { degree } = {} } = user;
+
+console.log(title, degree);
+
+const person = {
+  firstName: 'David',
+  lastName: 'Warner',
+  age: 31,
+};
+
+let { firstName, lastName } = person;
+console.log(firstName, lastName);
+
+//Array destructing
+const numbers = [43, 2, 21, [444, 432, 900], 34, 98];
+
+const [, , , [, a, b], , c] = numbers;
+
+console.log(a, b, c);
+
+//Swapping values
+let x = 32;
+let y = 43;
+
+[x, y] = [y, x];
+
+console.log(x, y);
