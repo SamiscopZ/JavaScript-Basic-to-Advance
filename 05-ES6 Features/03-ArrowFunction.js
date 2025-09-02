29d88f4763d68b8025c48bdee0fdc55962a1aa20
Samiul Islam <samiul.srss@gmail.com>
2025-09-02 23:14:24 +0600

    One more example added

--- 05-ES6 Features/03-ArrowFunction.js
+++ 05-ES6 Features/03-ArrowFunction.js
@@ -1,4 +1,11 @@
 //Arrow Function
+
+let myArrow = (a, b) => a + b;
+
+console.log(myArrow(54, 26));
+console.log(myArrow(54, 26), myArrow.length); //.length will return, how many parameter in this function.
+
+//If you need only one parameter, you can use  this way
 const printThis = a => {
   console.log('Printing this...', a);
 };
